ed796043e1c4e70414bf298723adff71cb30fd4f
Chris Geekie <chris.geekie@gmail.com>
2021-04-27 12:33:51 +0200

    Improved seeding script

--- seed_db.rb
+++ seed_db.rb
@@ -1,36 +1,36 @@
 require 'net/http'
 require 'uri'
 
-def random_set(iterations)
+def random_access(iterations, path)
   iterations.times do
-    name = ('a'..'z').to_a.sample
-    num = rand(1..100)
-    uri = URI.parse("http://localhost:4000/set?#{name}=#{num}")
-    response = Net::HTTP.get(uri)
-    p response
-  end
-end
-
-def random_get(iterations)
-  iterations.times do
-    name = ('a'..'z').to_a.sample
-    uri = URI.parse("http://localhost:4000/get?key=#{name}")
+    name = ('a'..'z').to_a.sample + ('a'..'z').to_a.sample # Random two-letter name
+    if path == 'set' 
+      num = rand(1..100)
+      query = "#{name}=#{num}"
+    elsif path == 'get'
+      query = "key=#{name}"
+    end
+    uri = URI.parse("http://localhost:4000/#{path}?#{query}")
     response = Net::HTTP.get(uri)
     p response
   end
 end
 
 if ARGV.length >= 1
+  if !ARGV.include?('--set') && !ARGV.include?('--get')
+    puts 'Usage: ruby seed_db.rb --set | --get'
+    return
+  end
   if ARGV.include?('--set')
-    puts "How many to randomly set?"
+    puts "How many keys to randomly set?"
     iterations = STDIN.gets.chomp.to_i
-    random_set(iterations)
+    random_access(iterations, 'set')
   end
   if ARGV.include?('--get')
-    puts "How many to randomly try to get?"
+    puts "How many keys to randomly try to get?"
     iterations = STDIN.gets.chomp.to_i
-    random_get(iterations)
+    random_access(iterations, 'get')
   end
 else
-  puts "Too few arguments, use --set and/or --get"
+  puts "Too few arguments, use [--set | --get]"
 end
